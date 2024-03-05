7d28c5e72f7e83080cecc5a953fe4b20ee8cf9d9
Abdul Raffay <abdulraffay2494@gmail.com>
2024-03-05 23:18:37 +0500

    New functions added

---
+++ Stacks & Queues/Stacks.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+public class Stacks {
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<Integer>();		
+		// stack.push(23);
+		// stack.push(43);
+		// stack.push(45);
+		// stack.push(56);
+		// System.out.println(stack.pop());
+		// System.out.println(stack.pop());
+		// System.out.println(stack.pop());
+		// System.out.println(stack.pop());
+		// System.out.println(stack.pop());
+
+
+		// Queue<Integer> queue = new LinkedList<>();
+		// queue.add(3);
+		// queue.add(5);
+		// queue.add(7);
+		// queue.add(9);
+
+		// System.out.println(queue.remove());
+		// System.out.println(queue.remove());
+		// System.out.println(queue.remove());
+		// System.out.println(queue.remove());
+	}
+}
