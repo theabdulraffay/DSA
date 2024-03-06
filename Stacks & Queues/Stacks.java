58b8a218cd48bfbb9bae09c884752a38131ad6a5
Abdul Raffay <abdulraffay2494@gmail.com>
2024-03-06 15:19:08 +0500

    Stacks adde

--- Stacks & Queues/Stacks.java
+++ Stacks & Queues/Stacks.java
@@ -1,9 +1,59 @@
-import java.util.Stack;
-import java.util.Queue;
-import java.util.LinkedList;
+// import java.util.Stack;
+// import java.util.Queue;
+// import java.util.LinkedList;
+
+class CustomStack{
+	protected int[] data;
+	private static final int DEFAULT_SIZE = 10;
+	private int ptr = -1;
+
+	public CustomStack(){
+		this(DEFAULT_SIZE);
+	}
+	public CustomStack(int size){
+		data = new int[size];
+	}
+
+	public boolean isEmpty(){
+		return ptr == -1;
+	}
+
+	public boolean isFull(){
+		return ptr == data.length - 1;
+	}
+
+	public boolean push(int item){
+		if(isFull()){
+			return false;
+		}
+
+		ptr++;
+		data[ptr] = item;
+		return true;
+	}
+
+	public int pop() throws Exception {
+		if(isEmpty()){
+			throw new Exception("Stack is Empty!!");
+		}
+		// int remove = data[ptr];
+		// data[ptr] = 0;
+		// ptr--;
+		// return remove;
+		return data[ptr--];
+	}
+
+	public int peak() throws Exception{
+		if(isEmpty()){
+			throw new Exception("Stack is Empty!!");
+		}
+		return data[ptr];
+	}
+
+}
 public class Stacks {
 	public static void main(String[] args) {
-		Stack<Integer> stack = new Stack<Integer>();		
+		// Stack<Integer> stack = new Stack<Integer>();		
 		// stack.push(23);
 		// stack.push(43);
 		// stack.push(45);
