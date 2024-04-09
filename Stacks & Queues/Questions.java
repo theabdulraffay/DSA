09b00e4dbde76da6ba3538eb012f325066e52f1b
Abdul Raffay <abdulraffay2494@gmail.com>
2024-04-09 22:31:58 +0500

    Questions added

--- Stacks & Queues/Questions.java
+++ Stacks & Queues/Questions.java
@@ -272,6 +272,31 @@ class Questions {
         
     }
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // https://leetcode.com/problems/time-needed-to-buy-tickets
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        // int sold = 0;
+        // while (tickets[k] != 0) {
+        //     for (int i = 0; i < tickets.length; i++) {
+        //         if (tickets[i] != 0) {
+        //             tickets[i] = tickets[i] - 1;
+        //             sold++;
+        //         }
+        //         if (tickets[k] == 0) break;
+        //     }
+        // }
+        // return sold;
+
+        int time = 0;
+        for (int i = 0; i < tickets.length; i++) {
+            if (i <= k) {
+                time += Math.min(tickets[k], tickets[i]);
+            } else {
+                time += Math.min(tickets[i], tickets[k] - 1);
+            }
+        }
+        return time;
+        
+    }
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
