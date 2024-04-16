e14377e511fc0810d14fa11f2fedc2d040586c86
Abdul Raffay <abdulraffay2494@gmail.com>
2024-04-16 23:52:39 +0500

    Functions added

--- Recursion/4. Subset, Subsequence, String/Permutation.java
+++ Recursion/4. Subset, Subsequence, String/Permutation.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 class Permutation {
 	public static void main(String[] args) {
 		String s = "abcd";
@@ -62,5 +63,41 @@ class Permutation {
 	// https://leetcode.com/problems/combination-sum/
 	// https://leetcode.com/problems/combination-sum-ii/
 	// https://leetcode.com/problems/palindrome-partitioning/
+	
+    //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+	// https://leetcode.com/problems/next-permutation/
+    public void nextPermutation(int[] nums) {
+        int index = -1;
+        for (int i = nums.length -2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+        if (index  == -1) {
+            Arrays.sort(nums);
+            return;
+        }
+        for (int i = nums.length -1; i > index; i--) {
+            if (nums[i] > nums[index]) {
+                int t = nums[i];
+                nums[i] = nums[index];
+                nums[index] = t;
+                break;
+            }
+        }
+
+        int i = index + 1;
+        int j = nums.length - 1;
+        while (i < j) {
+            int t = nums[i];
+            nums[i] = nums[j];
+            nums[j] = t;
+            i++;
+            j--;
+        }
+        
+    }
+
 
 }
