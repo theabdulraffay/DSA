44512a00610477c22c8c6a9dcf0eda49d795dda5
Abdul Raffay <abdulraffay2494@gmail.com>
2024-06-05 20:43:27 +0500

    Binary tree

--- Trees/QuestionsDFS.java
+++ Trees/QuestionsDFS.java
@@ -744,6 +744,51 @@ class BST {
         helperLargetValues(root.right, depth + 1);
     }
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // https://leetcode.com/problems/deepest-leaves-sum/description/
+    public int deepestLeavesSum2(TreeNode root) { // Using iteration
+        Queue<TreeNode> que = new LinkedList<>();
+        que.add(root);
+        int toret = 0;
+        while(!que.isEmpty()) {
+            int n = que.size();
+            int sum = 0;
+            for(int i = 0; i < n; i++) {
+                TreeNode te = que.poll();
+                sum += te.val;
+                if(te.left != null) que.add(te.left);
+                if(te.right != null) que.add(te.right);
+            }
+            toret = sum;
+        }
+        return toret;
+    }
+
+
+
+
+    int heightLeafSum(TreeNode root) {
+        if(root == null) return 0;
+        int l = heightLeafSum(root.left);
+        int r = heightLeafSum(root.right);
+
+        return Math.max(l, r) + 1;
+    }
+
+    int leaveSum = 0;
+    void sum(TreeNode root, int depth, int height) {
+        if(root == null) return;
+        if(depth == height) {
+            leaveSum += root.val;
+        }
+        sum(root.left, depth + 1, height);
+        sum(root.right, depth + 1, height);
+    }
+    public int deepestLeavesSum(TreeNode root) { // Using recursion
+        int n = heightLeafSum(root);
+        sum(root, 1, n);
+        return leaveSum;
+    }
+
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
