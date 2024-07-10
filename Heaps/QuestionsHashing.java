b39b67746af199417bd2414fbde0ac4e9ad5a700
Abdul Raffay <abdulraffay2494@gmail.com>
2024-07-10 16:18:54 +0500

    Hashing

--- Heaps/QuestionsHashing.java
+++ Heaps/QuestionsHashing.java
@@ -579,6 +579,27 @@ class QuestionsHashing {
     }
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // https://leetcode.com/problems/repeated-dna-sequences/
+    public List<String> findRepeatedDnaSequences(String s) {
+
+        int len = 10;
+        List<String> res = new ArrayList<String>();
+        if(s.length() < 10) return res;
+        
+        HashSet<String> set = new HashSet<>();
+        HashSet<String> added = new HashSet<>();
+        for (int i = 0; i <= s.length() - len; i++) {
+            String resStr = s.substring(i, i + len);
+            if (set.contains(resStr) && !added.contains(resStr)) {
+                added.add(resStr);
+            }
+            set.add(resStr);
+        }
+        res.addAll(added);
+        return res;
+
+    }
+
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
