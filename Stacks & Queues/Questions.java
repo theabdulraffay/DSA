7aff44871a6db4d1526671eba4fb96842ac79c55
Abdul Raffay <abdulraffay2494@gmail.com>
2024-07-11 21:14:35 +0500

    Stacks Questions

--- Stacks & Queues/Questions.java
+++ Stacks & Queues/Questions.java
@@ -354,6 +354,35 @@ class Questions {
     }
 
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/description/?envType=daily-question&envId=2024-07-11
+    void helper(Stack<Character>  st) { // will reverse the string till the last parenthesis
+        Queue<Character> que = new LinkedList<>();
+        while(st.peek() != '(') {
+            que.offer(st.pop());
+        } 
+        st.pop();
+        while(!que.isEmpty()) {
+            st.push(que.poll());
+        }
+    }
+
+    public String reverseParentheses(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if(c == ')') {
+                helper(st);
+            } else {
+                st.push(c);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+    }
+
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
