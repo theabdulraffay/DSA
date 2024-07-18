d94fdb759657ecf883ab8a5873521754f333b592
Abdul Raffay <abdulraffay2494@gmail.com>
2024-07-18 23:41:47 +0500

    1984

--- Trees/QuestionsDFS.java
+++ Trees/QuestionsDFS.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.HashMap;
 //https://www.geeksforgeeks.org/top-50-tree-coding-problems-for-interviews/
 //https://www.geeksforgeeks.org/top-50-tree-coding-problems-for-interviews/
 //https://www.geeksforgeeks.org/top-50-tree-coding-problems-for-interviews/
@@ -997,7 +998,7 @@ public boolean hasPathSum(TreeNode root, int targetSum) { // AMAZON QUESTION
 
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+    // https://leetcode.com/problems/delete-nodes-and-return-forest/
     List<TreeNode> toret;
     HashSet<Integer> set;
     TreeNode recursion(TreeNode root) {
@@ -1023,6 +1024,59 @@ public boolean hasPathSum(TreeNode root, int targetSum) { // AMAZON QUESTION
     }
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // https://leetcode.com/problems/number-of-good-leaf-nodes-pairs/
+    int k, count;
+    HashMap<TreeNode, List<Integer>> map;
+    boolean recursionCountPair(TreeNode root) {
+        if(root == null) return false;
+        if(root.left == null && root.right == null) return true;
+        boolean left = recursionCountPair(root.left);
+        boolean right = recursionCountPair(root.right);
+        if(left && right) {
+            List<Integer> l = another(root.left, 1);
+            List<Integer> r = another(root.right, 1);
+
+            for(int i = 0; i < l.size(); i++) {
+                for(int j = 0; j < r.size(); j++) {
+                    if(l.get(i) + r.get(j) <= k) { // Use 
+                        count++;
+                    }
+                }
+            }
+
+            List<Integer> lo = new ArrayList<>();
+            lo.addAll(r); lo.addAll(l);
+            map.put(root, lo);        
+        }
+        return left || right;
+    }
+
+    List<Integer> another(TreeNode root, int length) {
+        if(root == null) return null;
+        if(map.containsKey(root)) {
+            // List<Integer> list = new ArrayList<>();
+            List<Integer> toret = map.get(root);
+            for(int i = 0; i < toret.size(); i++) {
+                toret.set(i, toret.get(i) + length);
+            }
+            return toret;
+
+        }
+        if(root.left == null && root.right == null) {
+            return Arrays.asList(length);
+        }
+        List<Integer> left = another(root.left, length + 1);
+        if(left != null) return left;
+        return another(root.right, length + 1);
+    }
+    public int countPairs(TreeNode root, int distance) {
+        k = distance;
+        count = 0;
+        map = new HashMap<>();
+        recursionCountPair(root);
+        return count;
+    }
+
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
