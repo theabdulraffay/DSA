ae749336d66bbb7a9e13b6665cd61b6e5453fa72
Abdul Raffay <abdulraffay2494@gmail.com>
2024-07-23 00:03:10 +0500

    0380

--- Heaps/QuestionsHashing.java
+++ Heaps/QuestionsHashing.java
@@ -801,6 +801,42 @@ class QuestionsHashing {
     }
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // https://leetcode.com/problems/insert-delete-getrandom-o1/
+class RandomizedSet {
+    HashMap<Integer, Integer> map;
+    List<Integer> value;
+    Random random;
+    public RandomizedSet() {
+        value = new ArrayList<>();
+        map = new HashMap<>();
+        random = new Random();
+    }
+    
+    public boolean insert(int val) {
+        if(map.containsKey(val)) return false;
+        map.put(val, value.size());
+        value.add(val);
+        return true;
+        
+    }
+    
+    public boolean remove(int val) {
+        if(!map.containsKey(val)) return false;
+        int index = map.get(val);
+        map.put(value.get(value.size() - 1), index);
+        map.remove(val);
+        value.set(index, value.get(value.size() - 1));
+        value.remove(value.size() - 1);
+        return true;
+        
+    }
+    
+    public int getRandom() {
+        int i = random.nextInt(value.size());
+        return value.get(i);
+    }
+}
+
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
