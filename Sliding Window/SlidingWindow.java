fbf86347636edb1d656a0abf5ba8a34bdf5c8237
Abdul Raffay <abdulraffay2494@gmail.com>
2024-07-29 00:39:36 +0500

    1004

--- Sliding Window/SlidingWindow.java
+++ Sliding Window/SlidingWindow.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.HashSet;
 class SlidingWindow {
 	// https://leetcode.com/problems/maximum-average-subarray-i/
@@ -175,7 +177,31 @@ class SlidingWindow {
     }
 
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // https://leetcode.com/problems/max-consecutive-ones-iii/description/
+    public int longestOnes(int[] nums, int k) {
+        if(k == nums.length) return k;
+        List<Integer> list = new ArrayList<>();
+        int max = 0;
+        for(int i= 0; i < nums.length; i++) {
+            if(nums[i] == 0) list.add(i);
+        }
+        list.add(nums.length);
+        if(k >= list.size()) return nums.length;
+
+        
+        max = Math.max(max, list.get(k));
+
+        for(int i = k + 1; i < list.size(); i++) {
+            int ind = i - k - 1;
+            int nu = list.get(i) - (list.get(ind) + 1);
+            max = Math.max(max, nu);
+        }
+        return max;
+    }
+
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
