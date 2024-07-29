29fa6064d6f0726c7825233976e87b3f6ea5abef
Abdul Raffay <abdulraffay2494@gmail.com>
2024-07-29 22:21:04 +0500

    1493

--- Sliding Window/SlidingWindow.java
+++ Sliding Window/SlidingWindow.java
@@ -229,6 +229,25 @@ class SlidingWindow {
     }
 
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/description/
+    public int longestSubarray(int[] nums) {
+        int max = 0;
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++) { // store index of all the zeros and then compare their index to get the max length
+            if(nums[i] == 0) list.add(i);
+        }
+        list.add(nums.length);
+        if(list.size() < 2) return nums.length - 1;
+
+        max = Math.max(max, list.get(1) - 1);
+        for(int i = 2; i < list.size(); i++){
+            int num = list.get(i) - list.get(i - 2) - 2;
+            max = Math.max(max, num);
+        }
+        return max;
+        
+    }
+
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
