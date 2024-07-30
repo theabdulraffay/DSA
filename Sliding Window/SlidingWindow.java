5ef906888fb29dc8fad1f2923cb2844679458fbc
Abdul Raffay <abdulraffay2494@gmail.com>
2024-07-30 21:59:44 +0500

    424

--- Sliding Window/SlidingWindow.java
+++ Sliding Window/SlidingWindow.java
@@ -249,6 +249,42 @@ class SlidingWindow {
     }
 
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // https://leetcode.com/problems/longest-repeating-character-replacement/
+    int helper(String s, int k, char c) { // for every character we check the maximun length of substring with atmost k replacement of characters (same as 'longestSubarray' question)
+        List<Integer> list = new ArrayList<>();
+        int max = 0;
+        for(int i = 0; i < s.length() ; i++) {
+            if(s.charAt(i) != c) {
+                list.add(i);
+            }
+        }
+        list.add(s.length());
+        if(list.size() <= k) return s.length();
+
+        max = Math.max(max, list.get(k));
+        for(int i = k + 1; i < list.size(); i++) {
+            int num = list.get(i) - (list.get(i - k - 1) + 1);
+            max = Math.max(max, num);
+        }
+        return max;
+    }
+    public int characterReplacement(String s, int k) {
+        HashSet<Character> set = new HashSet<>();
+        for(char c : s.toCharArray()) {
+            set.add(c);
+        }
+        List<Character> arr = new ArrayList<>(set);
+        int max = 0;
+        for(char c : arr) {
+            int n = helper(s, k, c);
+            max = Math.max(max, n);
+
+        }
+        return max;
+        
+    }
+
+    //----------------------------------------------------------------------------------------------------------------------------------------------------------------
     // https://leetcode.com/problems/minimum-size-subarray-sum/
     public int minSubArrayLen(int target, int[] nums) {
         int i = 0;
@@ -277,6 +313,25 @@ class SlidingWindow {
         }
 
         return min;
+
+
+
+        // int i = 0;
+        // int sum = 0;
+        // int left = 0;
+        // int min = Integer.MAX_VALUE;
+        // while(i <= nums.length) {
+
+        //     while(sum >= target) {
+        //         min = Math.min(min, i - left);
+        //         sum -= nums[left++];
+        //     }
+
+        //     if(i < nums.length) sum += nums[i];
+        //     i++;
+        // }
+        
+        // return min == Integer.MAX_VALUE? 0 : min;
     }
 
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
