ea1d3ab131b937d3bc235fa90e808f32912f26d2
Abdul Raffay <abdulraffay2494@gmail.com>
2024-07-31 23:22:44 +0500

    1658

--- Sliding Window/SlidingWindow.java
+++ Sliding Window/SlidingWindow.java
@@ -361,6 +361,34 @@ class SlidingWindow {
     }
 
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/
+    public int minOperations(int[] nums, int x) { // we j find the total sum and then subtract 'x' from this, now we j have to find subarray of max length we that we can get minimun remaining indexes that would be our minimun operations for the 'x'
+        int tsum = 0;
+        for(int i : nums) tsum += i;
+        if(tsum < x) return -1;
+        int i = 0; 
+        int left = 0;
+        int sum = 0;
+        int max = -1;
+        int tofind = tsum - x;
+        while(i < nums.length && sum < tofind){
+            sum += nums[i++];
+        } 
+        if(sum == x) max = Math.max(max, i);
+
+        while(i <= nums.length) {
+            while(sum > tofind) {
+                sum -= nums[left++];
+            }
+            if(sum == tofind) max = Math.max(max, i - left);
+            if(i < nums.length)sum += nums[i];
+            i++;
+        }
+        return max == -1 ? -1 : nums.length - max;
+
+        
+    }
+
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
