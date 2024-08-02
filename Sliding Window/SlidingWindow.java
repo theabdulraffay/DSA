39c3733cf8f5f466dd59e0bc2f3fb7c4e27a9b28
Abdul Raffay <abdulraffay2494@gmail.com>
2024-08-02 23:17:22 +0500

    Sliding window

--- Sliding Window/SlidingWindow.java
+++ Sliding Window/SlidingWindow.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashSet;
+import java.util.HashMap;
 class SlidingWindow {
 	// https://leetcode.com/problems/maximum-average-subarray-i/
     public double findMaxAverage(int[] nums, int k) {
@@ -451,6 +452,30 @@ class SlidingWindow {
     }
 
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // https://leetcode.com/problems/maximum-sum-of-almost-unique-subarray/description/
+    public long maxSum(List<Integer> nums, int m, int k) {
+        HashMap<Integer, Integer> set = new HashMap<>();
+        long sum = 0;
+        long max = 0;
+        for(int i = 0; i < k; i++) {
+            sum += nums.get(i);
+            set.put(nums.get(i), set.getOrDefault(nums.get(i), 0) + 1);
+        }
+        for(int i = k; i < nums.size(); i++) {
+            if(set.size() >= m) max = Math.max(max, sum);
+            int prev = nums.get(i - k);
+            int forward = nums.get(i);
+            sum -= (long)prev;
+            sum += (long)forward;
+            set.put(prev, set.getOrDefault(prev, 0) - 1);
+            set.put(forward, set.getOrDefault(forward, 0) + 1);
+            if(set.get(prev) == 0) set.remove(prev);
+        }
+        if(set.size() >= m) max = Math.max(max, sum);
+        return max;
+        
+    }
+
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
