5136d8526d454c5a9a5876600279e7e0140cff4f
Abdul Raffay <abdulraffay2494@gmail.com>
2024-08-08 16:48:22 +0500

    1248

--- Sliding Window/SlidingWindow.java
+++ Sliding Window/SlidingWindow.java
@@ -310,6 +310,26 @@ class SlidingWindow {
         
     }
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // https://leetcode.com/problems/count-number-of-nice-subarrays/description/
+    public int numberOfSubarrays(int[] nums, int k) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++) if(nums[i] % 2 == 1) list.add(i);
+
+        if(k > list.size()) return 0;
+        list.add(nums.length);
+        int num = 0;
+        int n1 = list.get(k) - list.get(k - 1);
+        int n2 = list.get(0) + 1;
+        num += (n1 * n2);
+        for(int i = k + 1; i < list.size(); i++) {
+            n1 = list.get(i) - list.get(i - 1);
+            n2 = list.get(i - k) - list.get(i - k - 1);
+            num += (n1 * n2);
+        }
+        return num;
+    }
+
+    //----------------------------------------------------------------------------------------------------------------------------------------------------------------
     // https://leetcode.com/problems/minimum-size-subarray-sum/
     public int minSubArrayLen(int target, int[] nums) {
         int i = 0;
