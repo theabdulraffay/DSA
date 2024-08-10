5ee9318c3ca6a3deff97c64a41cd56e431badadd
Abdul Raffay <abdulraffay2494@gmail.com>
2024-08-10 11:59:49 +0500

    Sliding window

--- Sliding Window/SlidingWindow.java
+++ Sliding Window/SlidingWindow.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.HashSet;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Collections;
 class SlidingWindow {
 	// https://leetcode.com/problems/maximum-average-subarray-i/
@@ -575,6 +576,28 @@ class SlidingWindow {
         
     }
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // https://leetcode.com/problems/subarrays-with-k-different-integers/
+    int helper(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int c = 0;
+        int left = 0;
+
+        for(int i =0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+
+            while(map.size() > k) {
+                int m = nums[left++];
+                map.put(m, map.get(m) - 1);
+                if(map.get(m) == 0) map.remove(m);
+            }
+            c += (i - left + 1);
+        }     
+        return c;
+    }
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return helper(nums, k) - helper(nums, k - 1);
+    }
+
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
