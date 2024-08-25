699b9da096a6a085d3ad1212c930cfc9b2346c1e
Abdul Raffay <abdulraffay2494@gmail.com>
2024-08-26 00:04:32 +0500

    Sliding window

--- Sliding Window/SlidingWindow.java
+++ Sliding Window/SlidingWindow.java
@@ -776,6 +776,92 @@ class SlidingWindow {
     }
 
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // https://leetcode.com/problems/maximum-beauty-of-an-array-after-applying-operation/description/
+    public int maximumBeauty(int[] pair, int k) {
+        Arrays.sort(pair);
+        int left = 0;
+        int max = 0;
+
+        for(int i = 0; i < pair.length; i++) {
+            while(pair[i] - k > pair[left] + k) {
+                left++;
+            }
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
+    }
+
+    public int maximumBeauty2(int[] pair, int k) {
+        Arrays.sort(pair);
+        int n = pair.length;
+        // Pair[] pair = new Pair[n];
+        // for(int i = 0; i < nums.length; i++) {
+        //     int p = nums[i];
+        //     pair[i] = new Pair(p - k, p + k);
+        // }
+        // System.out.println(Arrays.toString(pair));
+        // int max = 0;
+        int left = 0;
+        int i = 0;
+        int fir = pair[0] + k;
+        while(i < n && pair[i] - k <= fir) {
+            i++;
+        }
+        if(i == n) return n;
+        int max = i;
+
+        while(i < n) {
+            while(pair[i] - k > pair[left] + k) {
+                left++;
+            }
+            max = Math.max(max, i - left + 1);
+            i++;
+        }
+        return max;
+    }
+
+    class Pair{
+        int fir;
+        int sec;
+        Pair(int x, int y) {
+            fir = x;
+            sec = y;
+        }
+
+        public String toString() {
+            return fir + " " + sec;
+        }
+    }
+
+
+    public int maximumBeauty3(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        Pair[] pair = new Pair[n];
+        for(int i = 0; i < nums.length; i++) {
+            int p = nums[i];
+            pair[i] = new Pair(p - k, p + k);
+        }
+        // System.out.println(Arrays.toString(pair));
+        // int max = 0;
+        int left = 0;
+        int i = 0;
+        int fir = pair[0].sec;
+        while(i < n && pair[i].fir <= fir) {
+            i++;
+        }
+        if(i == n) return n;
+        int max = i;
+
+        while(i < n) {
+            while(pair[i].fir > pair[left].sec) {
+                left++;
+            }
+            max = Math.max(max, i - left + 1);
+            i++;
+        }
+        return max;
+    }
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------
