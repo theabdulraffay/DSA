51f8f17ada9522b4e592d31479a9e541aaea754c
Abdul Raffay <abdulraffay2494@gmail.com>
2024-10-24 23:07:08 +0500

    DFS

--- Trees/QuestionsDFS.java
+++ Trees/QuestionsDFS.java
@@ -1078,6 +1078,31 @@ public boolean hasPathSum(TreeNode root, int targetSum) { // AMAZON QUESTION
     }
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// https://leetcode.com/problems/flip-equivalent-binary-trees/description/
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if(root1 == null && root2 == null) return true;
+        if(root1 == null || root2 == null) return false;
+        if(root1.val != root2.val) return false;
+            boolean temp = flipEquiv(root1.left, root2.left) || flipEquiv(root1.left, root2.right);
+            if(!temp) return false;
+            return flipEquiv(root1.right, root2.right) || flipEquiv(root1.right, root2.left);
+    }
+
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
