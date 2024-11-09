b32099a3ce62d9c76b8693843e8a7e6ea28347e8
Abdul Raffay <abdulraffay2494@gmail.com>
2024-11-09 20:46:51 +0500

    Hashing Questions

--- Heaps/QuestionsHashing.java
+++ Heaps/QuestionsHashing.java
@@ -884,6 +884,44 @@ class RandomPick {
     }
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// https://leetcode.com/problems/longest-consecutive-sequence/
+    public int longestConsecutive(int[] nums) {
+        if(nums.length == 0) return 0;
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+        HashSet<Integer> set = new HashSet<>();
+        int c = 1;
+        int max = 0;
+        for(int i : nums) if(!set.contains(i)) {set.add(i);que.add(i);}
+        while(!que.isEmpty()) {
+            int i = que.poll();
+            if (set.contains(i + 1)) c++;
+            else {max = Math.max(max, c);c = 1;}
+        }
+        return max;
+    }
+
+    public int longestConsecutive2(int[] nums) {
+        if(nums.length == 0) return 0;
+        Arrays.sort(nums);
+        int c = 1;
+        int max = 0;
+        int i = 1;
+        while(i < nums.length) {
+            if(nums[i] == nums[i - 1]) {
+                i++;
+                continue;
+            }
+
+            if(nums[i] - nums[i - 1] == 1) c++;
+            else {
+                max = Math.max(max, c);
+                c = 1;
+            }
+            i++;
+        }
+        return Math.max(max, c);   
+    }
+
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
