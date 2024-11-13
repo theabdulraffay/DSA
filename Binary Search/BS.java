748fe87061b0cf938d391ca05b7963bafd46b91c
Abdul Raffay <abdulraffay2494@gmail.com>
2024-11-13 23:55:44 +0500

    Binary Search

--- Binary Search/BS.java
+++ Binary Search/BS.java
@@ -396,6 +396,35 @@ public class BS{
     }
 
 	// -------------------------------------------------------------------------------------------------------------------------------------------
+	// https://leetcode.com/problems/count-the-number-of-fair-pairs/description/
+    int find(int[] nums, int x, int st, int en, boolean t) {
+        while(st < en) {
+            int mid = st + (en -st) / 2;
+            if(t) {
+                if(nums[mid] >= x) en = mid;
+                else st = mid + 1;
+            }
+            if(!t) {
+                if(nums[mid] > x) en = mid;
+                else st = mid + 1;
+            }
+        }
+        return st;
+    }
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        long c = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if(nums[i] + nums[n-1] < lower) continue;
+            int min = find(nums, lower - nums[i], i + 1, n, true);
+            int max = find(nums, upper - nums[i], i + 1, n, false);
+             c += (max - min );
+        }
+        return c;
+
+    }
+
 	// -------------------------------------------------------------------------------------------------------------------------------------------
 	// -------------------------------------------------------------------------------------------------------------------------------------------
 	// -------------------------------------------------------------------------------------------------------------------------------------------
